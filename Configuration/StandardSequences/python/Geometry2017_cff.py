9c889bf17b5fff1181280b99c05bb71c5e239138
Angira Rastogi <rastogiangira0908@gmail.com>
2016-12-29 10:15:13 +0100

    Phase-I geometry

---
+++ Configuration/StandardSequences/python/Geometry2017_cff.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Geometry.GeometryExtendedPhaseIPixel_cff import *
+from Configuration.Geometry.GeometryExtended2017newReco_cff import *
